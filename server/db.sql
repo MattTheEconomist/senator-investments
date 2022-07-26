50331a021f14cb0627fe0af09614ad72d13d00fc
Matt Ashford <mattashford1@gmail.com>
2022-07-26 14:36:10 -0400

    server and db created

--- server/db.sql
+++ server/db.sql
@@ -1,16 +1,42 @@
-CREATE TABLE trades( transaction_id SERIAL PRIMARY KEY, 
- transaction_date DATATYPE VARCHAR(15),
- disclosure_date DATATYPE VARCHAR(15),
- owner DATATYPE VARCHAR(15),
-ticker DATATYPE VARCHAR(15),
-asset_description DATATYPE VARCHAR(255),
-asset_type DATATYPE VARCHAR(55),
-type DATATYPE VARCHAR(50),
-amount DATATYPE VARCHAR(50),
-comment DATATYPE VARCHAR(100),
-senator DATATYPE VARCHAR(100),
- 
+CREATE TABLE trades( 
+   -- transaction_id SERIAL PRIMARY KEY, 
+ transaction_date  VARCHAR(15),
+ disclosure_date  VARCHAR(15),
+ owner VARCHAR(15),
+ticker  VARCHAR(15),
+asset_description  VARCHAR(255),
+asset_type  VARCHAR(55),
+type  VARCHAR(50),
+amount  VARCHAR(50),
+comment VARCHAR(100),
+senator  VARCHAR(100)
+ptr_link VARCHAR(255)
    );
+
+
+DROP TABLE IF EXISTS trades;
+
+
+
+COPY trades( 
+   -- transaction_id, 
+ transaction_date,
+ disclosure_date ,
+ owner ,
+ticker ,
+asset_description,
+asset_type ,
+type ,
+amount ,
+comment ,
+senator, 
+ptr_link )
+-- FROM 'C:\Users\Public\all_transactions.csv'
+FROM 'C:\Users\Public\test.csv'
+DELIMITER ','
+CSV HEADER;
+
+
 
 
 
--- server/server-index.js
+++ server/server-index.js
@@ -1,11 +1,59 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 // middlewarre
 
 app.use(cors());
+
+app.use(express.json());
+
+// ROUTES//
+
+//trade query
+app.get("/trades", async (req, res) => {
+  try {
+    let queryString = "SELECT * from trades";
+
+    let matches = [];
+
+    const requestQuery = req.query;
+
+    const column = Object.keys(requestQuery).pop();
+    const value = Object.values(requestQuery).pop();
+
+    if (typeof column !== "undefined") {
+      const filterCriteria = ` WHERE ${column}=$1`;
+
+      queryString += filterCriteria;
+      matches = await pool.query(queryString, [value]);
+    } else {
+      matches = await pool.query(queryString);
+    }
+
+    res.json(matches.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// get a transaction by id
+
+app.get("/trades/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const thisTrade = await pool.query(
+      "SELECT * from trades WHERE transaction_id=$1",
+      [id]
+    );
+    res.json(thisTrade.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 const PORT = 5000;
 
