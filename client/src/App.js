1999b952920d8194f7f2e1912a2814898533b1d4
Matt Ashford <mattashford1@gmail.com>
2022-07-27 10:53:38 -0400

    created client side

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import FilterCriteria from "./components/FilterCriteria";
+
+function App() {
+  return (
+    <>
+      <FilterCriteria />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/FilterCriteria.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+const FilterCriteria = () => {
+  const amounts = [
+    "$1,001 - $15,000",
+    "$15,001 - $50,000",
+    "$50,001 - $100,000",
+    "$100,001 - $250,000",
+    "$250,001 - $500,000",
+    "$500,001 - $1,000,000",
+    "$1,000,001 - $5,000,000",
+    "$5,000,001 - $25,000,000",
+    "$25,000,001 - $50,000,000",
+    "Over $50,000,000",
+    "Unknown",
+  ];
+
+  const optionItems = amounts.map((amount) => {
+    return (
+      <option key={`key${amount}`} value={`${amount}`}>
+        {amount}
+      </option>
+    );
+  });
+
+  const onSelect = async (e) => {
+    const amountSelected = e.target.value;
+    const column = "amount";
+    console.log("fetching", amountSelected);
+
+    e.preventDefault();
+
+    try {
+      let fetchString = `http://localhost:5000/trades`;
+      fetchString += `?${column}='${amountSelected}'`;
+
+      console.log("fetch String is", fetchString);
+
+      const response = await fetch(fetchString);
+      const jsonData = await response.json();
+      console.log(jsonData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <select className="form-select" onChange={onSelect}>
+        {optionItems}
+      </select>
+    </>
+  );
+};
+
+export default FilterCriteria;
