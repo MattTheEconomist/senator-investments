6c844ec2e61894254c614bbb887df54bcde04035
Matthew Ashford <mattashford1@gmail.com>
2022-12-01 17:27:05 -0500

    working on sql query to account for more transaciotns

--- server/db.sql
+++ server/db.sql
@@ -1,50 +1,44 @@
-CREATE TABLE trades( 
-   -- transaction_id SERIAL PRIMARY KEY, 
- transaction_date  VARCHAR(15),
- disclosure_date  VARCHAR(15),
- owner VARCHAR(15),
-ticker  VARCHAR(15),
-asset_description  VARCHAR(255),
-asset_type  VARCHAR(55),
-type  VARCHAR(50),
-amount  VARCHAR(50),
-comment VARCHAR(255),
-senator  VARCHAR(100),
-ptr_link VARCHAR(255)
-   );
-
-
-DROP TABLE IF EXISTS trades;
-
-
-
-COPY trades( 
-   -- transaction_id, 
- transaction_date,
- disclosure_date ,
- owner ,
-ticker ,
-asset_description,
-asset_type ,
-type ,
-amount ,
-comment ,
-senator, 
-ptr_link )
-FROM 'C:\Users\Public\all_transactions.csv'
--- FROM 'C:\Users\Public\test.csv'
-DELIMITER ','
-CSV HEADER;
-
-
-ALTER TABLE trades ADD COLUMN transaction_id SERIAL PRIMARY KEY;
-
-
-
-SELECT * FROM trades where amount= '$1,001 - $15,000' and senator='Debra S Fischer' limit 10;
--- SELECT * FROM trades WHERE amount = '$1,001 - $15,000' limit 10;
 
 
 
 
+with trans as(
+   SELECT transaction_date, type from transactions
+-- WHERE TO_DATE(transaction_date,'YYYY-MM-DD') BETWEEN '2021-04-07' and date '2021-04-07'+ interval '6 months' 
+-- AND 
+WHERE 
+"senatorId"=100
+AND ticker='OXY'
+)
+, hist as (
+   SELECT "date", "SPY", "OXY" from "historical_pricesH_R"  order by "date"
+)
+select hist."date",  hist."SPY", hist."OXY", trans.type, 
+trans.transaction_date
+ from hist 
+left join trans 
+on 
+trans.transaction_date = hist."date"
+-- select * from trans
+
+
+;
+
+
+
+-- select * from cte
+-- select historical_pricesH_R.OXY limit 1
+-- select "historical_pricesH_R"."OXY" limit 1
+
+
+--find senatorId column 
+
+select "senatorId" from transactions limit 1;
+
+
+
+-- select * from transactions where false ; 
+
+SELECT "date", "SPY", "${ticker}" from ${tableName} order by "date"
+SELECT "date", "SPY", "A" from "historical_pricesA_G" order by "date" limit 3; 
 
